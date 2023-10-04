8ee8ca3abe1df49be736f6a3b7cc71cd441904b2
Behnam Baharmand <bbaharmand@gmail.com>
2023-10-04 10:00:03 +0200

    Implement Gallery

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,5 +2,22 @@ import fetchImages from '@/lib/fetchImages'
 import type { ImagesResults } from '@/models/Images'
 
 export default async function Gallery() {
-  return <div>Gallery</div>
+  const url = 'https://api.pexels.com/v1/curated'
+
+  const images: ImagesResults | undefined = await fetchImages(url)
+
+  if (!images)
+    return (
+      <h2 className='m4 text-2xl font-bold'>There are not Images found!</h2>
+    )
+
+  return (
+    <section>
+      <ul>
+        {images.photos.map(photo => (
+          <li key={photo.id}>{photo.src.large}</li>
+        ))}
+      </ul>
+    </section>
+  )
 }
