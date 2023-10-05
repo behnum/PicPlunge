fb6e9243ea7ea2cac57f0e7260c381af7ccf40b1
Behnam Baharmand <bbaharmand@gmail.com>
2023-10-05 09:51:09 +0200

    Improve ImgContainers Fix styling, optimize via Next, apply sizes via ausi/RespImageLint

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,16 +7,17 @@ interface Props {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div className='relative h-64 bg-gray-800 rounded-xl'>
-      <div key={photo.id} className='h-64 bg-gray-800 rounded-xl'>
-        <Image
-          src={photo.src.large}
-          alt={photo.alt}
-          layout='fill'
-          objectFit='cover'
-          className='rounded-xl'
-        />
-      </div>
+    <div
+      key={photo.id}
+      className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'
+    >
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        sizes='(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)'
+        className='object-cover group-hover:opacity-75'
+      />
     </div>
   )
 }
